6c088a9747ce44f5cc0d433f9bc5903f2d6e5967
Gokula Krishnan <gokula.krishnan@achievers.com>
2019-09-21 18:04:01 -0400

    Goku: Async storage setup

--- app/Main.js
+++ app/Main.js
@@ -1,19 +1,7 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
 import { createAppContainer, StackActions, NavigationActions, createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
 
 import HomeScreen from './components/HomeScreen'
 import TDLoginScreen from './components/TDLoginScreen'
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
 
 const AppNavigator = createStackNavigator({
   Home: {
@@ -24,6 +12,8 @@ const AppNavigator = createStackNavigator({
   },
 }, {
     initialRouteName: 'Home',
+    mode: 'modal',
+    headerMode: 'none'
 });
 
 export default createAppContainer(AppNavigator);

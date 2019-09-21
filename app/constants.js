6572e8d204f156480ef05b13d2883666ed8a0c00
Gokula Krishnan <gokula.krishnan@achievers.com>
2019-09-21 16:56:58 -0400

    GOKU-donate-page

---
+++ app/constants.js
@@ -0,0 +1,11 @@
+import { Dimensions } from "react-native";
+
+module.exports = {
+  colorVars: {
+    appColor: '#FA9153',
+  },
+  dimenstions: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+};

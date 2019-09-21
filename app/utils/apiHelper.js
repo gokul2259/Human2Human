d77ab565079f0988e6f9c4ed8de0dacf5e1785c3
SK8007 <shaun.g.kho@gmail.com>
2019-09-21 17:01:15 -0400

    H2H: QRScanner

--- app/utils/apiHelper.js
+++ app/utils/apiHelper.js
@@ -1,26 +1,25 @@
-function setUpToken() {
-  return {
-    ...options,
-    headers: {
-      Authorization: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiN2E2ZjU5ZjgtYzJiMS0zN2RkLWE1MjktYjliYTU2MGE2ZDIzIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI3YTM2MzVhZS1lMjE0LTQ1NmMtYWQ4Ny00MmM4MTBkMWIyYzkifQ.UnuxUWpdUKy8htGP5JUrxSkB3iaV7474_zmBpii8fz4"
-    },
-  };
-}
+import { NativeModules } from "react-native";
 
-function fetchJSON(api, options) {
-  return fetch(new Request(`https:${api}`, setUpToken(options))).then(
-    response => response.json()
+const setUpToken = (options = {}) => ({
+  ...options,
+  headers: {
+    Authorization:
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiN2E2ZjU5ZjgtYzJiMS0zN2RkLWE1MjktYjliYTU2MGE2ZDIzIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiI3YTM2MzVhZS1lMjE0LTQ1NmMtYWQ4Ny00MmM4MTBkMWIyYzkifQ.UnuxUWpdUKy8htGP5JUrxSkB3iaV7474_zmBpii8fz4"
+  }
+});
+
+const fetchJSON = (api, options = {}) =>
+  fetch(new Request(`https://${api}`, setUpToken(options))).then(response =>
+    response.json()
   );
-}
 
-async function post(api, options = {}) {
-  const { body } = options;
-  await fetchJSON(api, {
-    method: "POST",
-    body: new FormData(body)
-  });
-}
+const post = async (api, { body = {} } = {}) =>
+  await fetchJSON(api, { method: "POST", body: new FormData(body) });
 
-async function get(api, options = {}) {
-  await fetchJSON(api, Object.assign({ method: "GET" }, { ...options }));
-}
+const get = async (api, options = {}) =>
+  await fetchJSON(api, { ...options, method: "GET" });
+
+module.exports = {
+  get,
+  post
+};
